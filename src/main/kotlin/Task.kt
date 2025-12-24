2ac28e8d6d6e4cfca2f66ba82024c0ea4eae9375
enriqueizel <eleaoizel@gmail.com>
2025-12-23 22:44:37 -0400

    refactor: fix the function name

--- src/main/kotlin/Task.kt
+++ src/main/kotlin/Task.kt
@@ -1,16 +1,16 @@
 import java.time.LocalDateTime
 
 data class Task(
-    val id: Int = gerenateID(),
+    val id: Int = generateID(),
     val title: String,
     val description: String?,
     val isCompleted: Boolean = false,
     val createdAt: LocalDateTime = LocalDateTime.now()
 ) {
     companion object {
-        private var idCount = 1
+        private var idCount = 0
 
-        fun gerenateID(): Int {
+        fun generateID(): Int {
             idCount += 1
             return idCount
         }

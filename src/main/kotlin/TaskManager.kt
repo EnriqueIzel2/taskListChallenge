ee2b243f8e0414cc3585f78d9f8c8c8c5e3ac8d0
enriqueizel <eleaoizel@gmail.com>
2025-12-27 22:45:15 -0400

    feat: add return in order to get task info

--- src/main/kotlin/TaskManager.kt
+++ src/main/kotlin/TaskManager.kt
@@ -1,8 +1,18 @@
 class TaskManager {
     private val taskList: MutableList<Task> = mutableListOf()
 
-    fun createTask(title: String, description: String?) {
+    fun createTask(title: String, description: String?) : Task {
         val newTask = Task(title = title, description = description)
         taskList.add(newTask)
+
+        return newTask
+    }
+
+    fun listTasks() {
+        taskList.forEach { task -> println("Task: ${task.title}") }
+    }
+
+    fun getTask(id: Int) : Task? {
+        return taskList.find { it.id == id}
     }
 }

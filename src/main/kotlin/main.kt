2bf915a1851cf322ac608944c925046e92fef3bf
enriqueizel <eleaoizel@gmail.com>
2025-12-27 22:45:36 -0400

    feat: add menu

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,53 @@
 fun main () {
-    var option: Int = 0
+    var option: Int? = 0
 
     val taskManager = TaskManager()
 
-    while (option != 1) {
-        println("Nome da tarefa")
-        val titleTask = readlnOrNull().toString()
+    while (option != 5) {
 
-        println("Descrição da tarefa")
-        val descriptionTask = readlnOrNull().toString()
+        println(
+            """
+        +---------------------------------+
+        |  CONTROLE DE TAREFAS |
+        +---------------------------------+
+        |  1 - Adicionar                  |
+        |  2 - Atualizar                  |
+        |  3 - Deletar                    |
+        |  4 - Buscar                     |
+        |  5 - Sair                       |
+        +---------------------------------+
+        """
+        )
+        option = readlnOrNull()?.toIntOrNull()
 
-        taskManager.createTask(titleTask, descriptionTask)
+        when(option) {
+            1 -> {
+                print("Titulo da task: ")
+                val title = readlnOrNull().toString()
+                print("descrição da task: ")
+                val description = readlnOrNull().toString()
 
-        println("a classe foi criada")
+                val taskCriada = taskManager.createTask(title = title, description = description)
 
-        option = 1
+                println("A tarefa #${taskCriada.id} foi criada com sucesso")
+            }
+            2 -> println("Aqui deve ATUALIZAR")
+            3 -> {
+                println("aqui deve DELETAR")
+            }
+            4 -> {println("aqui deve BUSCAR")}
+            5 -> {println("Aplicação encerrada.")}
+        }
+//        println("Nome da tarefa")
+//        val titleTask = readlnOrNull().toString()
+//
+//        println("Descrição da tarefa")
+//        val descriptionTask = readlnOrNull().toString()
+//
+//        taskManager.createTask(titleTask, descriptionTask)
+//
+//        println("a classe foi criada")
+
+
     }
 }
